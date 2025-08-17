d461a43fb404dc5cbb4d72bf2c1039ceed5797b6
Baratovysk <everaldocsantana@gmail.com>
2025-08-16 22:35:40 -0300

    Add files via upload

---
+++ app-config.js
@@ -0,0 +1,6 @@
+// app-config.js
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://hftnuncdwrhpgjmsvooe.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmdG51bmNkd3JocGdqbXN2b29lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzOTIwNzMsImV4cCI6MjA3MDk2ODA3M30.HAEnNVVAaO8xsicx10eCoOzkUN8TDbHw9RxH3mFA9ks",
+  PAGE_ID: null
+};
